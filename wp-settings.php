6ff278d4621ce1415787b56090f9cc3477f57465
simonefuoco <simofstudies@gmail.com>
2019-10-29 00:23:51 +0100

    wp-role autoloading

--- wp-settings.php
+++ wp-settings.php
@@ -133,7 +133,6 @@ wp_not_installed();
 // Load most of WordPress.
 require( ABSPATH . WPINC . '/capabilities.php' );
 
-require( ABSPATH . WPINC . '/class-wp-role.php' );
 require( ABSPATH . WPINC . '/class-wp-user.php' );
 require( ABSPATH . WPINC . '/class-wp-query.php' );
 require( ABSPATH . WPINC . '/query.php' );
